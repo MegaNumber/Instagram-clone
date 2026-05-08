311df9a3290f126a582f6c8c3d71f135eadbf244
MegaNumber <132915308+MegaNumber@users.noreply.github.com>
2026-05-08 20:20:17 +0330

    Update ffmpeg-utils.js

--- utils/ffmpeg-utils.js
+++ utils/ffmpeg-utils.js
@@ -1,23 +1,14 @@
 // مسیر فایل: /utils/ffmpeg-utils.js
-// توضیح: توابع کمکی برای پردازش ویدیو با FFmpeg.
-// شامل تولید thumbnail از ویدیو.
+// توضیح: تولید thumbnail از ویدیو و دریافت مدت زمان ویدیو با ffmpeg.
 
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegStatic = require('ffmpeg-static');
 const ffprobeStatic = require('ffprobe-static');
 const path = require('path');
 
-// تنظیم مسیر ffmpeg
 ffmpeg.setFfmpegPath(ffmpegStatic);
 ffmpeg.setFfprobePath(ffprobeStatic.path);
 
-/**
- * @function generateVideoThumbnail
- * @description تولید یک تصویر thumbnail از ویدیو
- * @param {string} videoPath - مسیر فایل ویدیو
- * @param {string} outputPath - مسیر خروجی thumbnail
- * @returns {Promise<string>} - مسیر فایل thumbnail
- */
 module.exports.generateVideoThumbnail = (videoPath, outputPath) => {
   return new Promise((resolve, reject) => {
     ffmpeg(videoPath)
@@ -28,16 +19,10 @@ module.exports.generateVideoThumbnail = (videoPath, outputPath) => {
         size: '640x360',
       })
       .on('end', () => resolve(outputPath))
-      .on('error', (err) => reject(err));
+      .on('error', reject);
   });
 };
 
-/**
- * @function getVideoDuration
- * @description دریافت طول مدت ویدیو به ثانیه
- * @param {string} videoPath - مسیر فایل ویدیو
- * @returns {Promise<number>} - مدت ویدیو به ثانیه
- */
 module.exports.getVideoDuration = (videoPath) => {
   return new Promise((resolve, reject) => {
     ffmpeg.ffprobe(videoPath, (err, metadata) => {
